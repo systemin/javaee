92b8b9e9732e669bd74651677fda388c67c7ec9a
admins <728413649@qq.com>
2018-06-11 14:22:47 +0800

    "FactoryPattern"

--- src/main/java/com/hello/pattern/FactoryPatternDemo.java
+++ src/main/java/com/hello/pattern/FactoryPatternDemo.java
@@ -3,13 +3,13 @@ package com.hello.pattern;
 public class FactoryPatternDemo {
   public static void main(String[] args) {
     ShapeFactory shapeFactory = new ShapeFactory();
-    FactoryPattern factoryPattern=shapeFactory.getFactoryPattern("FactoryPatternImpl");
+    FactoryPattern factoryPattern = shapeFactory.getFactoryPattern("FactoryPatternImpl");
     factoryPattern.draw();
 
-    FactoryPattern factoryPattern1=shapeFactory.getFactoryPattern("FactoryPatternImpl1");
+    FactoryPattern factoryPattern1 = shapeFactory.getFactoryPattern("FactoryPatternImpl1");
     factoryPattern1.draw();
 
-    FactoryPattern factoryPattern2=shapeFactory.getFactoryPattern("FactoryPatternImpl2");
+    FactoryPattern factoryPattern2 = shapeFactory.getFactoryPattern("FactoryPatternImpl2");
     factoryPattern2.draw();
   }
 }
